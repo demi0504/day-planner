b05805b222da40aed5a95998c2c7a2ecbfa1f2b3
demi0504 <ripley.demi@gmail.com>
2020-06-08 16:28:10 -0500

    added functions for todos and local storage

--- script.js
+++ script.js
@@ -1,88 +1,78 @@
-
-//Display current date
 $(document).ready(function(){
+    //Display current date
     const currentDate = moment().format("MMM Do YYYY");
     let $dateHeading = $('#currentDay');
     $dateHeading.text(currentDate);
 
-    
-    let workHours = ["9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm"];
+    //Array of time
+    var workHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+    var todosArray = [];
 
-    //Build rows for work hours 
-    var $container = $("#planner-container");
-    $.each(workHours, function (index, value) {
-       var backgroundColor = getTime(value)
-       var $newRow = $('<div>').addClass('row');
-       var $newCol1 = $('<div>' + value + '</div>').addClass('col-sm-1 hour time');
-       var $newCol2 = $('<div></div>').addClass('col-sm-10 past description '+backgroundColor+' time-' + value);
-       var $newCol3 = $('<button></button>').addClass('col-sm-1 saveBtn');
-       var textarea = $('<textarea></textarea>').addClass('col-sm-12 input');
-       var icon = $('<i class="far fa-save fa-3x" style="margin: auto; padding: 10px;"></i>');
-       $newRow.append($newCol1);
-       $newRow.append($newCol2);
-       $newCol2.append(textarea);
-       $newRow.append($newCol3);
-       $newCol3.append(icon);
-       $container.append($newRow);
-    })
-    $('.saveBtn').click(function () {
-       var time = $(this).siblings('div.time').text();
-       console.log(time);
-       var input = $(this).siblings('div.description').children("textarea").val();
-       console.log(input);
-       localStorage.setItem(time, input);
-    })
-   //storage///
-    //$('.time-9AM .input').val(localStorage.getItem('9AM'))
-    ///storage///
+    init();
+
+    //Build time blocks for work hours 
+    function renderTimeBlocks() {
+        for (var i = 9; i <= 17; i++){
+            let index = i - 9;
+            let timeBlock = $("<div></div>").addClass("time-block row");
+            let timeArea = $("<div></div>").addClass("hour col-2").text(workHours[index]);
+            let textArea = $("<textarea></textarea").addClass("input" + index + " description col-9").text("");
+            let button = $("<button></button>").addClass("saveBtn col-1").attr("data-btnIndex", index);
+            let saveIcon = $("<i></i>").addClass("fas fa-save fa-2x");
+            $("#planner-container").append(timeBlock);
+            timeBlock.append(timeArea, textArea, button);
+            button.append(saveIcon);
+
+            //Adjust color of time blocks
+            setRowColor(textArea, i);
+
+            textArea.text(todosArray[index]);
+        }
+    }
     
-    $.each(workHours, function (index, value) {
-       console.log(value)
-       $('.time-'+value+' .input').val(localStorage.getItem(value))
-    })
-   function getTime(current) {
-      var date = new Date();
-      var hours = date.getHours();
-      var minutes = date.getMinutes();
-      
-      // Check whether AM or PM
-      var newformat = hours >= 12 ? 'PM' : 'AM';  
-      
-      // Find current hour in AM-PM Format
-      hours = hours % 12;  
-      
-      // To display "0" as "12"
-      hours = hours ? hours : 12;  
-      minutes = minutes < 10 ? '0' + minutes : minutes;
-      var currentTime = hours + newformat;
-      
-      if(currentTime == current){
-        
-         return 'red'
-      }
-      if(hours == 12 && hours > parseInt(current) && current.includes('PM')){
-         return 'green'
-      }
-      if(current.includes('AM') && newformat == 'PM'){
+    //Function to set the color of time blocks
+    function setRowColor(row, hour) {
+        if(hour < parseInt(moment().format('H'))){
+            row.addClass("past");
+        } else if(hour > parseInt(moment().format('H'))){
+            row.addClass("future");
+        }else {
+            row.addClass("present");
+        }
+    }
    
-         return 'gray'
-      } else if(newformat == 'AM' && current.includes('PM')){
-         return 'green'
-      }
-      
-      if(current.includes(newformat)){
-         var currentNumber = parseInt(current.replace(newformat, ''))
-         if(currentNumber <= hours){
-            return 'gray'
-         } else if(currentNumber == 12){
-            return 'gray'
-         } else {
-            return 'green'
-         }
-      }
-      console.log(hours + newformat);
-      return ''
-   }
+    //Get todos from local storage
+    function init() {
+        var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+        if (storedTodos !== null) {
+            todosArray = storedTodos;
+        } else{
+            todosArray = new Array();
+            storeTodos();
+        }
+
+        renderTimeBlocks();
+    }
+
+    //Define stored todos
+    function storeTodos(){
+        localStorage.setItem("todos", JSON.stringify(todosArray));
+    }
+
+    $(".saveBtn").on("click",function(event){
+        event.preventDefault(); 
+        let index = $(this).attr("data-btnIndex");  
+        let todoText = $(".input"+index).val();
+
+        // Add new todoText to todos array
+        todosArray[index] = todoText;
+        console.log(todosArray);
+        
+        // Store updated todos in localStorage
+        storeTodos();
+
+    });
 })
 
 
